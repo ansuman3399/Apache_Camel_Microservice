f2a0cf525ef327d752b0d2c2d657dbec8f73ffcb
Ansuman Mohanty <ansumanm20@gmail.com>
2021-07-25 02:19:29 +0530

    Changing the file name

--- src/main/java/com/apache/Camel/CamelAApplication.java
+++ src/main/java/com/apache/Camel/CamelAApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CamelApplication {
+public class CamelAApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CamelApplication.class, args);
+		SpringApplication.run(CamelAApplication.class, args);
 	}
 
 }
